83543fc61071dab6ab8bf719c835fec80ad6714e
HugoAseka <hugomaseka@gmail.com>
2022-04-01 22:10:36 -0300

    whatsapp completo

--- driveneats.js
+++ driveneats.js
@@ -40,3 +40,27 @@ function barraInferior (){
         after.classList.remove("show");
     }
 }
+function whatsapp(){
+    
+let prato = document.querySelector(".selecionado1").querySelector("strong").innerHTML;
+let bebida = document.querySelector(".selecionado2").querySelector("strong").innerHTML;
+let sobremesa = document.querySelector(".selecionado3").querySelector("strong").innerHTML; 
+let pratoCusto = Number(document.querySelector(".selecionado1").querySelector("span").innerHTML);
+let bebidaCusto = Number(document.querySelector(".selecionado2").querySelector("span").innerHTML);
+let sobremesaCusto = Number(document.querySelector(".selecionado3").querySelector("span").innerHTML);
+let total = (pratoCusto + bebidaCusto + sobremesaCusto).toFixed(2);
+let nome, endereco = "";
+nome = prompt("Qual seu nome?");
+endereco = prompt("Qual seu endereço?")
+
+
+let uri = encodeURIComponent(`Olá, gostaria de fazer o pedido:
+- Prato: ${prato}
+- Bebida: ${bebida}
+- Sobremesa: ${sobremesa}
+Total: R$ ${total}
+
+Nome: ${nome}
+Endereço: ${endereco}`);
+document.querySelector(".bottom-after").href="https://wa.me/5544999831316?text="+uri;
+}
